9c9105599f292fe23bc98524b2024b27c025c3f0
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2026-09-10 12:59:05 +0530

    a tough one

---
+++ SortingAndSearching/ConcertTickets.cpp
@@ -0,0 +1,46 @@
+/*
+Author :
+
+███████╗████████╗ █████╗ ██████╗  ███╗   ███╗ █████╗ ███╗   ██╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗ ████╗ ████║██╔══██╗████╗  ██║
+███████╗   ██║   ███████║██████╔╝ ██╔████╔██║███████║██╔██╗ ██║
+╚════██║   ██║   ██╔══██║██║  ██║ ██║╚██╔╝██║██╔══██║██║╚██╗██║
+███████║   ██║   ██║  ██║██║  ██║ ██║ ╚═╝ ██║██║  ██║██║ ╚████║
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝  STARMAN248
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int> tiks;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        tiks.insert(x);
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+
+        auto it = tiks.upper_bound(x);
+
+        if (it == tiks.begin()) {
+            cout << -1 << '\n';
+        } else {
+            --it; 
+            cout << *it << '\n';
+            tiks.erase(it);
+        }
+    }
+
+    return 0;
+}
